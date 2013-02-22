b4716b0c470e7030f5117b9dbac184ce6fee3550
Tom Offermann <tom@offermann.us>
2013-02-22 01:01:35 -0800

    Just call setup() with named parameters.
      * No longer build up config dict.

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'name': 'pydiceware',
-    'description': 'Generate passphrases using the Diceware algorithm.',
-    'author': 'Tom Offermann',
-    'url': '',
-    'download_url': '',
-    'author_email': 'tom@offermann.us',
-    'version': '0.1.0',
-    'install_requires': ['randomSources'],
-    'packages': ['diceware'],
-    'package_data': {'diceware': ['data/beale.wordlist.asc',
-                                  'data/diceware.wordlist.asc',
-                                  'data/metadata.json']
-                    },
-    'scripts': ['bin/diceware-passphrase.py'],
-}
-
-setup(**config)
+setup(
+    name='pydiceware',
+    description='Generate passphrases using the Diceware algorithm.',
+    author='Tom Offermann',
+    url='',
+    download_url='',
+    author_email='tom@offermann.us',
+    version='0.1.0',
+    install_requires=['randomSources'],
+    packages=['diceware'],
+    package_data={'diceware': ['data/beale.wordlist.asc',
+                               'data/diceware.wordlist.asc',
+                               'data/metadata.json']
+                 },
+    scripts=['bin/diceware-passphrase.py'],
+)
